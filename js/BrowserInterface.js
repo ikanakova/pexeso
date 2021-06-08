648a08978e3d4c768cbc5732ec9d50a1f116e1e0
Wikinaut <mail@wikinaut.de>
2021-06-08 21:47:36 +0200

    upd

--- js/BrowserInterface.js
+++ js/BrowserInterface.js
@@ -18,7 +18,7 @@
 
   // Shuffle card images: How many different images are available to shuffle
   // from?
-  var imagesAvailable = 15;
+  var imagesAvailable = 28;
 
   /************ End hard coded settings ******************/
 
